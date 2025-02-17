7637bef22e5f33dcd56e9821e4414667a69956ba
shog50 <10709@holbertonstudents.com>
2025-02-17 11:37:49 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - Entry point
+ * Description: a program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char *str = "_putchar";
