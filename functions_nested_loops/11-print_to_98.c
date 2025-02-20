91a77306a0d04067f37ef096ff784b08cb3e20fc
shog50 <10709@holbertonstudents.com>
2025-02-20 13:04:16 +0000

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
+* print_to_98 - Prints all natural numbers from n to 98.
 * @n: The starting number.
 */
 void print_to_98(int n)
