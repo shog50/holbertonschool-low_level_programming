f3d5a40a888c43cf65506685b940439d8f728db3
shog50 <10709@holbertonstudents.com>
2025-02-26 11:32:27 +0000

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- pointers_arrays_strings/main_12.c
+++ pointers_arrays_strings/main_12.c
@@ -1,8 +1,33 @@
+#include "main.h"
 #include <stdio.h>
 
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-printf("Hello, World!\n");
-return 0;
+int nb;
+
+nb = _atoi("98");
+printf("%d\n", nb);
+nb = _atoi("-402");
+printf("%d\n", nb);
+nb = _atoi("          ------++++++-----+++++--98");
+printf("%d\n", nb);
+nb = _atoi("214748364");
+printf("%d\n", nb);
+nb = _atoi("0");
+printf("%d\n", nb);
+nb = _atoi("Suite 402");
+printf("%d\n", nb);
+nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+printf("%d\n", nb);
+nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+printf("%d\n", nb);
+nb = _atoi("-2147483648");  /* Test case for INT_MIN */
+printf("%d\n", nb);
+return (0);
 }
 
