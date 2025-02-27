a0ba13dc80bb44643c3f8ba0ea9335c076918355
shog50 <10709@holbertonstudents.com>
2025-02-27 21:33:18 +0000

    rot13

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
-char *rot13(char *str) {
+char *rot13(char *str)
+{
 char *p = str;
 char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 int i;
 
-while (*p) {
-for (i = 0; i < 52; i++) {
-if (*p == alph[i]) {
+while (*p)
+{
+for (i = 0; i < 52; i++)
+{
+if (*p == alph[i])
+{
 *p = rot13[i];
 break;
 }
