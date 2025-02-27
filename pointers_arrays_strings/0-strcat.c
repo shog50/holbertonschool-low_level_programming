1a10ef34584e80138e31cc764891044429feba47
shog50 <10709@holbertonstudents.com>
2025-02-27 11:29:15 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 * _strcat - concatenates two strings.
 *@dest: destination string.
 *@src: source string.
-* 
+*
 * Return: pointer to the resulting string dest
 */
 char *_strcat(char *dest, char *src)
@@ -23,5 +23,5 @@ j++;
 }
 dest[i] = '\0';
 
-return dest;
+return (dest);
 }
