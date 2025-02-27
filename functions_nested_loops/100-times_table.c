c7325ee37dbd61dd94e0ee6f6e5c1f67767748e4
shog50 <10709@holbertonstudents.com>
2025-02-27 12:35:50 +0000

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
 * print_times_table - prints the n times table, starting with 0
@@ -8,6 +9,7 @@
 void print_times_table(int n)
 {
 int i, j, result;
+char buffer[4];
 
 if (n < 0 || n > 15)
 return;
@@ -17,24 +19,38 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 result = i * j;
-if (j == 0)
+
+if (j != 0)
+write(1, ",", 1);
+
+if (result < 10)
 {
-printf("%d", result);
-}
-else if (result < 10)
-{
-printf(",   %d", result);
+if (j != 0)
+write(1, "   ", 3);
+buffer[0] = result + '0';
+buffer[1] = '\0';
 }
 else if (result < 100)
 {
-printf(",  %d", result);
+if (j != 0)
+write(1, "  ", 2);
+buffer[0] = (result / 10) + '0';
+buffer[1] = (result % 10) + '0';
+buffer[2] = '\0';
 }
 else
 {
-printf(", %d", result);
+if (j != 0)
+write(1, " ", 1);
+buffer[0] = (result / 100) + '0';
+buffer[1] = ((result / 10) % 10) + '0';
+buffer[2] = (result % 10) + '0';
+buffer[3] = '\0';
 }
+
+write(1, buffer, strlen(buffer));
 }
-printf("\n");
+write(1, "\n", 1);
 }
 }
 
