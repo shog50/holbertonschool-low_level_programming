0f1448d12ec134b66407ce22e56790d606acb625
shog50 <10709@holbertonstudents.com>
2025-03-05 19:23:27 +0000

    It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Prints the program's name
+* @argc: Argument count (unused)
+* @argv: Array of argument strings
+* Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
+
