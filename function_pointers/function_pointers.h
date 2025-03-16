3e6d749bea679c06530f5f29bc224524583d6e93
shog50 <10709@holbertonstudents.com>
2025-03-16 20:14:44 +0000

    What's my name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,8 +3,20 @@
 
 #include <stddef.h>
 
+/**
+ * print_name - Prints a name using a given function pointer.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that defines how the name is printed.
+ */
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c); /* If needed for `putchar` */
+
+/**
+ * _putchar - Writes a character to standard output.
+ * @c: The character to be written.
+ * Return: On success, returns 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
 
