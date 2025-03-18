8352c0b037662364801c3a412d8ec5503931b5dd
shog50 <10709@holbertonstudents.com>
2025-03-18 19:02:04 +0000

    Beauty is variable, ugliness is constant

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+
+/**
+ * print_name - Prints a name using a given function pointer.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that defines how the name is printed.
+ */
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+
+#endif /* FUNCTION_POINTERS_H */
+
