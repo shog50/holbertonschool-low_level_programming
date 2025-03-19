373805c6bc70dd8edc464a3ae08b1b8cff779185
shog50 <10709@holbertonstudents.com>
2025-03-19 08:37:43 +0000

    To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,24 +2,26 @@
 #include <stdio.h>
 
 /**
-* print_all - Prints anything based on the format provided.
-* @format: A list of types of arguments passed to the function.
-* Description: The function prints values corresponding to the format specifiers:
-* 'c' for char, 'i' for integer, 'f' for float, and 's' for string.
-* If a string argument is NULL, "(nil)" is printed instead.
+* print_all - Prints anything based on the format.
+* @format: A string listing the types of arguments.
+* Description: 'c' = char, 'i' = integer, 'f' = float, 's' = string.
+* Strings that are NULL are printed as "(nil)".
+* Any other character in the format string is ignored.
 */
 void print_all(const char * const format, ...)
 {
 va_list args;
 int i = 0;
-char *str;
-char *separator = "";
+char *str, *separator = "";
 
 va_start(args, format);
 
 while (format && format[i])
 {
-if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+if (format[i] == 'c' ||
+format[i] == 'i' ||
+format[i] == 'f' ||
+format[i] == 's')
 {
 printf("%s", separator);
 switch (format[i])
