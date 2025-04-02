917a7154cc27806f8c147eaa7ccb9218ea7b47af
shog50 <10709@holbertonstudents.com>
2025-04-02 20:13:59 +0000

    cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+* close_file - Closes a file descriptor and handles errors.
+* @fd: The file descriptor to close.
+*/
+void close_file(int fd)
+{
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+
+/**
+* main - Copies the content of a file to another file.
+* @argc: The number of arguments.
+* @argv: The arguments.
+*
+* Return: 0 on success, exits with codes 97, 98, 99, or 100 on failure.
+*/
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to, read_bytes, written_bytes;
+char buffer[BUFFER_SIZE];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+/* Open source file */
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+/* Open destination file */
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close_file(fd_from);
+exit(99);
+}
+
+/* Read from source and write to destination */
+while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+written_bytes = write(fd_to, buffer, read_bytes);
+if (written_bytes == -1 || written_bytes != read_bytes)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close_file(fd_from);
+close_file(fd_to);
+exit(99);
+}
+}
+
+if (read_bytes == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close_file(fd_from);
+close_file(fd_to);
+exit(98);
+}
+
+/* Close file descriptors */
+close_file(fd_from);
+close_file(fd_to);
+
+return (0);
+}
+
