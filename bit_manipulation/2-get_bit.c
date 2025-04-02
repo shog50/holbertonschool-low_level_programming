5b3e264031dd3de903316b10349e51e97761d4ba
shog50 <10709@holbertonstudents.com>
2025-04-02 18:39:42 +0000

    2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,4 +1,11 @@
 #include <limits.h>
+/**
+* get_bit - Returns the value of a bit at a given index.
+* @n: The number to search within.
+* @index: The index of the bit to retrieve, starting from 0.
+* Return: The value of the bit at the given index (0 or 1),
+*         or -1 if an error occurred (e.g., index out of range).
+*/
 
 int get_bit(unsigned long int n, unsigned int index)
 {
