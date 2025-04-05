84c4ecaf6313587ca6e1949aad2198883cbe6a84
shog50 <10709@holbertonstudents.com>
2025-04-05 18:11:29 +0000

    cp

--- file_io/main.h
+++ file_io/main.h
@@ -1,11 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void close_file(int fd);
+void copy_content(int fd_from, int fd_to, char *src, char *dest);
 
 #endif /* MAIN_H */
 
